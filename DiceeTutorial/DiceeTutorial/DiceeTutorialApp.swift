ad2d531e1a72142edb398d248d4adddd3a0d8c11
Joe Barrett <joe.barrett@image-space.com>
2024-11-15 15:48:57 +0000

    Create inital xcode project

---
+++ DiceeTutorial/DiceeTutorial/DiceeTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeTutorialApp.swift
+//  DiceeTutorial
+//
+//  Created by Joe Barrett on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceeTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
